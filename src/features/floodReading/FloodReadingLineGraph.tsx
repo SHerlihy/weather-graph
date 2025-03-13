ef56dc10ed56750d4212b11a716ef37cc1abd2a7
SHerlihy <65720475+SHerlihy@users.noreply.github.com>
2025-03-13 18:16:35 +0000

    render a layout (#5)

--- src/features/floodReading/FloodReadingLineGraph.tsx
+++ src/features/floodReading/FloodReadingLineGraph.tsx
@@ -19,47 +19,49 @@ export function FloodReadingLineGraph({
 }) {
     const floodReadingsContext = useFloodReadings()
     return (
-        <>
-            {!floodReadingsContext && <p>Flood records unavailable</p>}
-            {!floodReadingsContext?.floodReadings && <p>Flood records unavailable</p>}
-            {
-                floodReadingsContext?.floodReadings &&
-                < Card >
-                    <CardHeader>
-                        <CardTitle>{title}</CardTitle>
-                        <CardDescription>{description}</CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
-                            <LineChart
-                                accessibilityLayer
-                                data={floodReadingsContext.floodReadings}
-                                margin={{
-                                    left: 12,
-                                    right: 12,
-                                }}
-                            >
-                                <CartesianGrid vertical={false} />
-                                <XAxis
-                                    dataKey="time"
-                                    tickLine={true}
-                                    axisLine={true}
-                                    tickMargin={12}
-                                />
-                                <YAxis
-                                    type="number"
-                                />
-                                <Line
-                                    dataKey="value"
-                                    type="linear"
-                                    dot={false}
-                                />
-                            </LineChart>
-                        </ChartContainer>
-                    </CardContent>
-                </Card >
-            }
-        </>
+        <div className="fixed h-[60vh] w-full">
+            <div className="w-full h-full bg-current order-5">
+                {!floodReadingsContext && <p>Flood records unavailable</p>}
+                {!floodReadingsContext?.floodReadings && <p>Flood records unavailable</p>}
+                {
+                    floodReadingsContext?.floodReadings &&
+                    <Card className="p-0">
+                        <CardHeader>
+                            <CardTitle>{title}</CardTitle>
+                            <CardDescription>{description}</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+                                <LineChart
+                                    accessibilityLayer
+                                    data={floodReadingsContext.floodReadings}
+                                    margin={{
+                                        left: 12,
+                                        right: 12,
+                                    }}
+                                >
+                                    <CartesianGrid vertical={false} />
+                                    <XAxis
+                                        dataKey="time"
+                                        tickLine={true}
+                                        axisLine={true}
+                                        tickMargin={12}
+                                    />
+                                    <YAxis
+                                        type="number"
+                                    />
+                                    <Line
+                                        dataKey="value"
+                                        type="linear"
+                                        dot={false}
+                                    />
+                                </LineChart>
+                            </ChartContainer>
+                        </CardContent>
+                    </Card >
+                }
+            </div>
+        </div>
     )
 }
 
